97218de03b122f382ad209f5867bab7ed7131fee
Nikita Titov <nmtitov@yandex.ru>
2018-04-02 13:41:33 +0200

    Fix load file issues

--- src/mgla/hello.clj
+++ src/mgla/hello.clj
@@ -1,12 +1,9 @@
 (ns mgla.hello)
 
-(def user (create-user "Nikita" "Titov"))
-(def player {:user user :id 10})
-(def player2 {:user user :id 10})
-
 (defn create-user [first-name last-name]
     {:name first-name :last-name last-name})
 
+(def user (create-user "Nikita" "Titov"))
 
-(defn palindrome? [s]
-    (= s (clojure.string/join "" (reverse s))))
+(def player {:user user :id 10})
+(def player2 {:user user :id 10})
